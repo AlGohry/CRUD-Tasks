9e9c42b334bb7d05b339d19ff6b2f483a073e59e
Ahmed Abdellatif <ahmad.3bdellatif@gmail.com>
2024-02-29 16:15:00 +0300

    Login services

--- projects/admin/src/app/auth/components/login/login.component.ts
+++ projects/admin/src/app/auth/components/login/login.component.ts
@@ -1,27 +1,54 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private fb: FormBuilder) { }
-  loginForm!: FormGroup
+  constructor(
+    private fb: FormBuilder,
+    private service: LoginService,
+    private toaster: ToastrService,
+    private router: Router,
+    private spinner: NgxSpinnerService
+  ) { }
+  loginForm!: FormGroup;
   ngOnInit(): void {
-    this.createForm()
+    this.createForm();
   }
 
   createForm() {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]]
-    })
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+        ],
+      ],
+      role: ['admin'],
+    });
   }
-  login(){
-    console.log(this.loginForm.value);
-
+  login() {
+    this.spinner.show();
+    this.service.login(this.loginForm.value).subscribe(
+      () => {
+        this.toaster.success('Success', 'Login Success');
+        this.spinner.show();
+      },
+      () => {
+        this.toaster.error('Authentication field');
+        this.router.navigate(['/tasks'])
+        this.spinner.hide();
+      }
+    );
   }
 }
