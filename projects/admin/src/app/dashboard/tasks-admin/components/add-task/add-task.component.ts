08b33f0f56328cea46736d3449740adf07cbb41e
Ahmed Abdellatif <ahmad.3bdellatif@gmail.com>
2024-03-04 12:03:47 +0300

    Processing...

--- projects/admin/src/app/dashboard/tasks-admin/components/add-task/add-task.component.ts
+++ projects/admin/src/app/dashboard/tasks-admin/components/add-task/add-task.component.ts
@@ -1,22 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.scss']
+  styleUrls: ['./add-task.component.scss'],
 })
 export class AddTaskComponent implements OnInit {
+  constructor(
+    private fb: FormBuilder,
+    public dialog: MatDialogRef<AddTaskComponent>,
+    public matDialog: MatDialog
+  ) {}
 
-  constructor(private fb:FormBuilder , public dialog: MatDialogRef<AddTaskComponent> , public matDialog:MatDialog) { }
+  users: any = [
+    { name: 'Moahmed', id: 1 },
+    { name: 'Ali', id: 2 },
+    { name: 'Ahmed', id: 3 },
+    { name: 'Zain', id: 4 },
+  ];
 
-  users:any = [
-    {name:"Moahmed" , id:1},
-    {name:"Ali" , id:2},
-    {name:"Ahmed" , id:3},
-    {name:"Zain" , id:4},
-  ]
+  fileName = '';
+  newTaskForm!: FormGroup;
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.newTaskForm = this.fb.group({
+      title: ['', Validators.required],
+      userId: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required],
+      deadline: ['', Validators.required],
+    });
+  }
+
+  selectImage(event: any) {
+    this.fileName = event.target.value;
+    this.newTaskForm.get('image')?.setValue(event.target.files[0])
+    console.log(event);
+
+  }
+  createTask() {
+    console.log(this.newTaskForm.value);
   }
 }
--- projects/admin/src/app/dashboard/tasks-admin/services/tasks.service.ts
+++ projects/admin/src/app/dashboard/tasks-admin/services/tasks.service.ts
@@ -1,9 +1,18 @@
+import { CreateTask } from './../context/DTOs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllTasks() {
+    return this.http.get('https://crud-gsvs.onrender.com/tasks/all-tasks');
+  }
+
+  createTask(model: CreateTask) {
+    return this.http.post('https://crud-gsvs.onrender.com/tasks/add-tasks', model);
+  }
 }
